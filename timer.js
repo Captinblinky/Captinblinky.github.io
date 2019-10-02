bb69dfafb75ae594978edf70a044ff83339720c0
Leon <leon.chow1@ontariotechu.net>
2019-10-02 18:54:24 -0400

    Add timer JS file to control time logic

---
+++ timer.js
@@ -0,0 +1,15 @@
+function runTimer() {
+    let hour = document.getElementById("hour").value;
+    let min = document.getElementById("min").value;
+    let sec = document.getElementById("sec").value;
+    console.log(typeof hour);
+    console.log(typeof min);
+    console.log(typeof sec);
+    let timeRemaining = document.getElementById("timeRemaining");
+    if (!(+hour >= 0 && +hour <= 9999) || (!(+min >= 0 && +min <= 59)) || (!(+sec >= 0 && +sec <= 59))) {
+        timeRemaining.innerHTML = "Invalid time!";
+    } else {
+        timeRemaining.innerHTML = "";
+        console.log("works!");
+    }
+}
