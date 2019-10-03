52dfcfad2fc6ddf112f3c8a4fd2d533123304226
Leon <leon.chow1@ontariotechu.net>
2019-10-02 20:06:03 -0400

    add start button disable when timer is running, fix timer display

--- timer.js
+++ timer.js
@@ -2,21 +2,20 @@ function runTimer() {
     let hour = document.getElementById("hour").value;
     let min = document.getElementById("min").value;
     let sec = document.getElementById("sec").value;
-    console.log(typeof hour);
-    console.log(typeof min);
-    console.log(typeof sec);
     let timeRemaining = document.getElementById("timeRemaining");
-    if (!(+hour >= 0 && +hour <= 9999) || (!(+min >= 0 && +min <= 59)) || (!(+sec >= 0 && +sec <= 59))) {
+    timeRemaining.innerHTML = "Time Remaining: " + hour + ":" + min + ":" + sec;
+    if (!(+hour >= 0 && +hour <= 9999) || hour.length == 0 || (!(+min >= 0 && +min <= 59)) || min.length == 0 || !(+sec >= 0 && +sec <= 59) || sec.length == 0) {
         timeRemaining.innerHTML = "Invalid time!";
     } else {
+        document.getElementById("start").disabled = true;
         setInterval(function() {
+            timeRemaining.innerHTML = "Time Remaining: " + hour + ":" + min + ":" + sec;
             if (sec > 0) {
                 sec--;
             } else if (sec < 1 && min > 0) {
-                sec = 60; 
+                sec = 59; 
                 min--;
             } 
         }, 1000);
-        timeRemaining.innerHTML = hour + ":" + min + ":" + sec;
     }
 }
