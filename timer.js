2a090dc2a196d77c32cfdfafb2f109d1f4c36a23
Leon <leon.chow1@ontariotechu.net>
2019-10-02 20:46:44 -0400

    Add clear time button with implementation

--- timer.js
+++ timer.js
@@ -1,3 +1,5 @@
+let setTimer;
+
 function runTimer() {
     let timeRemaining = document.getElementById("timeRemaining");
     let hour = document.getElementById("hour").value;
@@ -10,7 +12,7 @@ function runTimer() {
     if (!(+hour >= 0 && +hour <= 9999) || hour.length < 1 || hour.length > 9999 || (!(+min >= 0 && +min <= 59)) || min.length < 1 || min.length > 2 || !(+sec >= 0 && +sec <= 59) || sec.length < 1 || sec.length > 2) {
         timeRemaining.innerHTML = "Invalid time!";
     } else {
-        let setTimer = setInterval(function() {
+        setTimer = setInterval(function() {
             timeRemaining.innerHTML = "Time Remaining: " + hour + ":" + min + ":" + sec;
             if (sec > 0) {
                 sec--;
@@ -29,4 +31,19 @@ function runTimer() {
         }, 1000);
         document.getElementById("start").disabled = true;
     }
+}
+
+function resetTimer() {
+    console.log("Resetting timer...");
+    clearInterval(setTimer);
+    let timeRemaining = document.getElementById("timeRemaining");
+    let hour = document.getElementById("hour");
+    let min = document.getElementById("min");
+    let sec = document.getElementById("sec");
+    
+    hour.value = "00";
+    min.value = "00";
+    sec.value = "00";
+    timeRemaining.innerHTML = "Timer cleared!";
+    document.getElementById("start").disabled = false;
 }
