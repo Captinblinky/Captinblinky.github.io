67b7fed8a8b7cbdd0e70b36ae3f5dde946acd187
Leon <leon.chow1@ontariotechu.net>
2019-10-02 20:39:30 -0400

    Add reference, finish timer logic and display

--- timer.js
+++ timer.js
@@ -1,21 +1,32 @@
 function runTimer() {
+    let timeRemaining = document.getElementById("timeRemaining");
     let hour = document.getElementById("hour").value;
     let min = document.getElementById("min").value;
     let sec = document.getElementById("sec").value;
-    let timeRemaining = document.getElementById("timeRemaining");
+    
+    timeRemaining.innerHTML = "";
+    
     timeRemaining.innerHTML = "Time Remaining: " + hour + ":" + min + ":" + sec;
-    if (!(+hour >= 0 && +hour <= 9999) || hour.length == 0 || (!(+min >= 0 && +min <= 59)) || min.length == 0 || !(+sec >= 0 && +sec <= 59) || sec.length == 0) {
+    if (!(+hour >= 0 && +hour <= 9999) || hour.length < 1 || hour.length > 9999 || (!(+min >= 0 && +min <= 59)) || min.length < 1 || min.length > 2 || !(+sec >= 0 && +sec <= 59) || sec.length < 1 || sec.length > 2) {
         timeRemaining.innerHTML = "Invalid time!";
     } else {
-        document.getElementById("start").disabled = true;
-        setInterval(function() {
+        let setTimer = setInterval(function() {
             timeRemaining.innerHTML = "Time Remaining: " + hour + ":" + min + ":" + sec;
             if (sec > 0) {
                 sec--;
-            } else if (sec < 1 && min > 0) {
+            } else if (min > 0) {
                 sec = 59; 
                 min--;
-            } 
+            } else if (hour > 0) {
+                min = 59;
+                hour--;
+            } else {
+                alert("Out of time!");
+                timeRemaining.innerHTML = "Out of time!";
+                document.getElementById("start").disabled = false;
+                clearInterval(setTimer);
+            }
         }, 1000);
+        document.getElementById("start").disabled = true;
     }
 }
